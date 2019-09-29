000217dd4264f93bf710c33728fc3d10e7d1f446
McZhD <53159590+McZhD@users.noreply.github.com>
2019-09-30 04:45:16 +0800

    Add files via upload

--- dicomviewer/dicomanal.py
+++ dicomviewer/dicomanal.py
@@ -11,6 +11,17 @@ import os
 
 
 def strufile(datadir):
+    """RT Structure Set Storage file Finder
+
+        Locate the RT Structure Set Storage File in the patient folder.
+
+        Args:
+            datadir: The path to the patient folder.
+
+        Returns:
+            If there is no structure file in the folder, returns a string. Else return
+            the path to the RT Structure Set Storage file.
+    """
     data = []
     strufilepath = 'No Structure File'
     for fpath, dirs, fs in os.walk(datadir):
@@ -26,6 +37,17 @@ def strufile(datadir):
 
 
 def dosefile(datadir):
+    """RT Dose Storage file Finder
+
+        Locate the RT Structure Set Storage File in the patient folder.
+
+        Args:
+            datadir: The path to the patient folder.
+
+        Returns:
+            If there is no dose storage file in the folder, returns a string. Else return
+            the path to the RT Dose Storage file.
+    """
     data = []
     dosefilepath = 'No Dose File'
     for fpath, dirs, fs in os.walk(datadir):
@@ -41,6 +63,17 @@ def dosefile(datadir):
 
 
 def planfile(datadir):
+    """RT Plan Storage file Finder
+
+        Locate the RT Structure Set Storage File in the patient folder.
+
+        Args:
+            datadir: The path to the patient folder.
+
+        Returns:
+            If there is no dose plan file in the folder, returns a string. Else return
+            the path to the RT Plan Storage file.
+    """
     data = []
     planfilepath = 'No Plan File'
     for fpath, dirs, fs in os.walk(datadir):
@@ -56,6 +89,16 @@ def planfile(datadir):
 
 
 def imgxyfile(datadir):
+    """CT Image Storage files Finder
+
+        Locate the CT Image Storage files in the patient folder.
+
+        Args:
+            datadir: The path to the patient folder.
+
+        Returns:
+            Return a list of the path to the CT Image Storage files.
+    """
     data = []
     imgxylist = []
     for fpath, dirs, fs in os.walk(datadir):
@@ -72,6 +115,18 @@ def imgxyfile(datadir):
 
 
 def imgxy(datadir, no):
+    """Image XY Pixel Array
+
+        Extract the XY plane Pixel Array of the designated CT slice.
+
+        Args:
+            datadir: The path to the patient folder.
+            no: The No. of the CT slice in the return of def imgxyfile, start from 1.
+
+        Returns:
+            Return a the XY plane Pixel Array of the designated CT slice and the patient position of the slice.
+            ([number of rows],[number of columns]), [x, y, z]
+    """
     if no > len(imgxyfile(datadir)):
         return 'Slice Number Out Of Range'
     imgset = pydicom.dcmread(imgxyfile(datadir)[no-1])
@@ -81,6 +136,20 @@ def imgxy(datadir, no):
 
 
 def imgxystru(datadir, no):
+    """Image XY Structure information
+
+        Extract the Structure information the designated CT slice.
+
+        Args:
+            datadir: The path to the patient folder.
+            no: The No. of the CT slice in the return of def imgxyfile, start from 1.
+
+        Returns:
+            List of names of the structures, List of colors of the structures,
+            List of contour coordinates of the structures
+           [name1, name2, ...], [[r_name1, g_name1, b_name1,...],
+           [[[[list of name1_contour1_x],[list of name1_contour1_y], [list of name1_contour1_z], ...], ...], ...]
+    """
     strupath = strufile(datadir)
     slicpos = pydicom.dcmread(imgxyfile(datadir)[no - 1]).ImagePositionPatient
     pixspc = pydicom.dcmread(imgxyfile(datadir)[no - 1]).PixelSpacing
@@ -119,6 +188,18 @@ def imgxystru(datadir, no):
 
 
 def imgxydose(datadir, no):
+    """Image XY Dose Distribution information
+
+        Extract the Dose Distribution information the designated CT slice.
+
+        Args:
+            datadir: The path to the patient folder.
+            no: The No. of the CT slice in the return of def imgxyfile, start from 1.
+
+        Returns:
+            Levels of dose, Colors of the levels, dose distribution, Labels of the dose.
+            [level1, level2, ...], [color1, color2, ...], ([number of rows],[number of columns]), [label1, label2, ...]
+    """
     dosepath = dosefile(datadir)
     if dosepath == 'No Dose File':
         print(dosepath)
@@ -162,6 +243,17 @@ def imgxydose(datadir, no):
 
 
 def drawimgxy(datadir, no, stru=False, dose=False):
+    """Draw Image XY
+
+        Draw and save the designated XY slice.
+
+        Args:
+            datadir: The path to the patient folder.
+            no: The No. of the CT slice in the return of def imgxyfile, start from 1.
+            stru: Whether draw the contours of structure on the XY slice.
+            dose: Whether draw the contours of dose distribution on the XY slice.
+
+    """
     slice = imgxy(datadir, no)[0]
     plt.figure(figsize=(11, 6))
     plt.title('Slice Position Z = ' + str(imgxy(datadir, no)[1][2]))
@@ -196,6 +288,18 @@ def drawimgxy(datadir, no, stru=False, dose=False):
 
 
 def rgbnorm(rgb):
+    """Normalize RGB values from [0, 255] to [0, 1]
+
+        In the dcm files, the RGB values are [0, 255], but in matplotlib are [0, 1]
+
+        Args:
+            rgb: The rgb list. [r, g, b]
+
+        return:
+            Normalized RGB values.
+            [r_0to1, g_0to1, b_0to1]
+
+    """
     norm = []
     for item in range(0, 3):
         norm.append(rgb[item]/255)
